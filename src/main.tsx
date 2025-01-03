a4306a29ffea1fe1e8611e4e3deff359bd087c3e
Alex <zemzikova.oleksandra@gmail.com>
2025-01-03 14:23:24 +0200

    base2

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
